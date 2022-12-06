01f8fa6a528d6ef1e03ba8a39d45a1e50b8a973d
ellrub <ellefsen88@gmail.com>
2022-12-06 22:33:34 +0100

    styling

--- frontend_react/src/container/Produkter/Produkter.jsx
+++ frontend_react/src/container/Produkter/Produkter.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
+import {AiFillEye, AiFillGithub} from "react-icons/ai"
 
 import { AppWrap, MotionWrap } from "../../wrapper"
 import "./Produkter.scss";
@@ -7,33 +8,73 @@ import { urlFor, client } from "../../client";
 
 const Produkter = () => {
     const [produkter, setProdukter] = useState([])
+    const [activeFilter, setActiveFilter] = useState("All")
+    const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
+    const [filterProdukter, setFilterProdukter] = useState([])
 
     useEffect(() => {
         const query = '*[_type == "produkter"]'
 
         client.fetch(query)
-            .then((data) => setProdukter(data))
+            .then((data) => {
+            setProdukter(data)
+            setFilterProdukter(data);
+        })
     }, [])
-    
 
+    const handleProdukterFilter = (item) => {
+        setActiveFilter(item);
+        setAnimateCard([{y: 100, opacity: 0}])
+
+        setTimeout(() => {
+            setAnimateCard([{y: 0, opacity: 1}])
+
+            if(item === "Alle") {
+                setFilterProdukter(produkter);
+            } else {
+                setFilterProdukter(produkter.filter((produkter) => produkter.tags.includes(item)))
+            }
+
+        }, 500);
+    }
+    
     return (
-    <>
-        <h2 className='app__headline-text'>Produkter</h2>
-        <div className='app__profiles'>
-            {produkter.map((produkter, index) => (
-                <motion.div
-                    whileInView={{opacity: 1}}
-                    transition={{duration: 0.5, type: "tween"}}
-                    className="app__profile-item"
-                    key={produkter.title + index}
-                >
-                    <img src={urlFor(produkter.imgUrl)} alt={produkter.title} />
-                    <h2 className='app__produkter-item-h2' style={{marginTop: 20}}>{produkter.title}</h2>
-                    <p className='app__produkter-item-p' style={{marginTop: 10}}>{produkter.description}</p>
-                </motion.div>
-            ))}
-        </div>
-    </>
+        <>
+            <h2 className='app__produkter-head'>Produkter</h2>
+            <div className='app__produkter-filter'>
+                {["Juleøl", "Alle"].map((item, index) => (
+                    <div 
+                    key={index}
+                    onClick={() => handleProdukterFilter(item)}
+                    className={`app__produkter-filter-item app__flex p-text ${activeFilter === item ? "item-active" : ""}`}
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
+            <motion.div
+                animate={animateCard}
+                transition={{duration: 0.5, delayChildren:0.5}}
+                className="app__produkter-portfolio"
+                // whileHover={{scale: 1.04}}
+            >
+                {filterProdukter.map((produkter, index) => (
+                    <div className='app__produkter-item app__flex' key={index}>
+                        <div className='app__produkter-img app__flex'>
+                            <img src={urlFor(produkter.imgUrl)} alt={produkter.name} />
+                        </div>
+                        <div className='app__produkter-content app__flex'>
+                            <h4 className='bold-text'>{produkter.title}</h4>
+                            <p style={{marginTop: 10}}>{produkter.description}</p>
+                            <div className='app__produkter-tag app__flex'>
+                                <p>{produkter.tags[0]}</p>
+    
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </motion.div>
+        </>
 )}
 
 export default AppWrap(
